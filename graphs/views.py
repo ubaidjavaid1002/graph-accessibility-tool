219e21fa968d057fb3dc7a14ff72a15bd3a34d2d
ubaid <ubaidjavaid1002@gmail.com>
2026-05-20 01:37:15 +0500

    implemented OCR text extraction

--- graphs/views.py
+++ graphs/views.py
@@ -1,9 +1,15 @@
 from django.shortcuts import render
 from .forms import GraphUploadForm
+import pytesseract
+from PIL import Image
+
+pytesseract.pytesseract.tesseract_cmd = r'C:\Program Files\Tesseract-OCR\tesseract.exe'
+
 
 def home(request):
 
     uploaded_image = None
+    extracted_text = None
 
     if request.method == 'POST':
 
@@ -18,11 +24,16 @@ def home(request):
 
             uploaded_image = graph.image.url
 
+            image = Image.open(graph.image.path)
+
+            extracted_text = pytesseract.image_to_string(image)
+
     else:
 
         form = GraphUploadForm()
 
     return render(request, 'graphs/home.html', {
         'form': form,
-        'uploaded_image': uploaded_image
+        'uploaded_image': uploaded_image,
+        'extracted_text': extracted_text
     })
