1154b23973091407df9bb36684fb7db53a747989
ubaid <ubaidjavaid1002@gmail.com>
2026-06-01 16:31:06 +0500

    added image preprocessing for OCR

--- graphs/views.py
+++ graphs/views.py
@@ -1,6 +1,10 @@
 from django.shortcuts import render
 from .forms import GraphUploadForm
+
 import pytesseract
+import cv2
+import numpy as np
+
 from PIL import Image
 
 pytesseract.pytesseract.tesseract_cmd = r'C:\Program Files\Tesseract-OCR\tesseract.exe'
@@ -24,10 +28,17 @@ def home(request):
 
             uploaded_image = graph.image.url
 
-            image = Image.open(graph.image.path)
+            # using opencv to preprocess the image before extracting text
+            # cv2 reads image as matrix
+            image = cv2.imread(graph.image.path)
 
-            extracted_text = pytesseract.image_to_string(image)
+            # convert to grayscale
+            gray = cv2.cvtColor(image,cv2.COLOR_BGR2GRAY)
 
+            # apply thresholding to get binary image
+            thresh = cv2.threshold(gray,150,255,cv2.THRESH_BINARY)[1]
+
+            extracted_text = pytesseract.image_to_string(thresh)
     else:
 
         form = GraphUploadForm()
